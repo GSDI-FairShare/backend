970d3e89710dd95945c306b28e249819ca4e71bf
rluizaga <rluizaga@fi.uba.ar>
2024-05-16 02:16:08 -0300

    se termina los endpoints para el login

--- src/controllers/auth_controllers.py
+++ src/controllers/auth_controllers.py
@@ -5,6 +5,8 @@ from src.database.connection import get_db
 from src.services.auth_service import AuthService
 from src.models.request.user import User, UserCreate
 from src.auth.hash_password import HashPassword
+from src.models.response.token import TokenResponse
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -17,3 +19,11 @@ def sign_user_up(user: UserCreate, db: Session = Depends(get_db)):
     hashed_password = hash_password.create_hash(user.password)
     user.password = hashed_password
     return auth_service.signup(user)
+
+
+@router.post("/signin", response_model=TokenResponse)
+def sign_user_in(
+    user: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+):
+    auth_service: AuthService = AuthService(db)
+    return auth_service.signin(user)
--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -1,6 +1,12 @@
+from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 from src.services.user_service import UserService
 from src.models.request.user import UserCreate
+from src.auth.hash_password import HashPassword
+from src.auth.jwt_handler import create_access_token
+from fastapi.security import OAuth2PasswordRequestForm
+
+hash_password = HashPassword()
 
 
 class AuthService:
@@ -9,3 +15,20 @@ class AuthService:
 
     def signup(self, user: UserCreate):
         return self.user_service.create(user)
+
+    def signin(self, user: OAuth2PasswordRequestForm):
+        user_db = self.user_service.find_by_email(user.username)
+        if not user_db:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect email or password",
+            )
+
+        if not hash_password.verify_hash(user.password, user_db.password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect email or password",
+            )
+
+        access_token = create_access_token(user_db.email)
+        return {"access_token": access_token, "token_type": "Bearer"}
