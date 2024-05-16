24d6d69d472839438ee229fb168e3df65c6bb140
rluizaga <rluizaga@fi.uba.ar>
2024-05-16 11:34:03 -0300

    se modifica el connection

--- src/database/connection.py
+++ src/database/connection.py
@@ -3,17 +3,17 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-# import os
+import os
 
 load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-# SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False},
-    # SQLALCHEMY_DATABASE_URL
+    # SQLALCHEMY_DATABASE_URL,
+    # connect_args={"check_same_thread": False},
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
