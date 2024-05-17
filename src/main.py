275ea026c8c4c0d57151691c39cb23ec78752319
rluizaga <rluizaga@fi.uba.ar>
2024-05-17 20:24:38 -0300

    se quita rutas que no usamos

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from src.controllers import user_controllers
+# from src.controllers import user_controllers
 from src.controllers import auth_controllers
 from src.controllers import individual_expenses_controllers
 from src.database.connection import Base, engine
@@ -8,6 +8,6 @@ from src.database.connection import Base, engine
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-app.include_router(user_controllers.router)
+# app.include_router(user_controllers.router)
 app.include_router(auth_controllers.router)
 app.include_router(individual_expenses_controllers.router)
